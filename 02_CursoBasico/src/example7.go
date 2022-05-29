ab572faa78343e8c504bfd10016ce9c59fd8e387
Julio Cesar Maldonado <jcmaldonado@asesoftware.com>
2022-05-29 00:27:59 -0500

    examples

---
+++ 02_CursoBasico/src/example7.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+type pc struct {
+	ram   int
+	disk  int
+	brand string
+}
+
+func (myPc pc) String() string {
+	return fmt.Sprintf("Tengo %d DB RAM, %d GB Disco y es una %s", myPc.ram, myPc.disk, myPc.brand)
+}
+
+func main() {
+
+	myPc := pc{ram: 16, disk: 100, brand: "msi"}
+	fmt.Println(myPc)
+}
--- 03_CursoBasico/main.go
+++ 03_CursoBasico/main.go
@@ -5,6 +5,20 @@ import (
 	"fmt"
 )
 
+type pc struct {
+	ram   int
+	disk  int
+	brand string
+}
+
+func (myPc pc) ping() {
+	fmt.Println(myPc.brand, "Pong")
+}
+
+func (myPc *pc) duplicateRAM() {
+	myPc.ram = myPc.ram * 2
+}
+
 func main() {
 	var myCar pk.CarPublic
 	myCar.Brand = "Ferrari"
@@ -13,4 +27,22 @@ func main() {
 
 	pk.PrintMessage("Hola")
 
+	a := 50
+	b := &a
+
+	fmt.Println(a)
+	fmt.Println(*b)
+
+	myPc := pc{ram: 16, disk: 1000, brand: "msi"}
+	fmt.Println(myPc)
+
+	myPc.ping()
+
+	fmt.Println(myPc)
+	myPc.duplicateRAM()
+
+	fmt.Println(myPc)
+	myPc.duplicateRAM()
+
+	fmt.Println(myPc)
 }
